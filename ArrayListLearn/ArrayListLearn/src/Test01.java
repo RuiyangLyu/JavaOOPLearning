6cdb4a6ef85c7e59460dbe924a6dc189ea23a59c
BakaHui <56708414+RuiyangLyu@users.noreply.github.com>
2022-12-05 19:31:21 +0800

    作业12.5更新

--- ArrayListLearn/ArrayListLearn/src/Test01.java
+++ ArrayListLearn/ArrayListLearn/src/Test01.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Test01 {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            String temp = i+"";
+            arr.add(temp);
+        }
+        String temp = arr.get((int) (10 * Math.random()));
+        System.out.println(temp);
+    }
 }
---
+++ ArrayListLearn/ArrayListLearn/src/phoneCodeReader.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class phoneCodeReader {
+    public static final String FilePath = "C:\\Users\\MSI\\Documents\\GitHub\\JavaOOPLearning\\ArrayListLearn\\ArrayListLearn\\src\\phonecode.txt";//文件目录
+    private static Map<String, String> hashMap = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        File2HashMap();
+        System.out.println(hashMap);
+    }
+
+    public static void File2HashMap() throws IOException {
+        File file = new File(FilePath);
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String data = null;
+            while ((data = reader.readLine()) != null){
+                String[] param = data.split(",");
+                hashMap.put(param[0],param[1]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try{
+                reader.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
